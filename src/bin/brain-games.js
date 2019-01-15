5c695a0af7e2cffbc6b2173f971dc6114590b278
Dmitrii Pochtennov <dmitrii.pochtennov@gmail.com>
2019-01-15 16:42:01 +0100

    Fixes after the first mentor-check

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-import names from "..";
-names();
+import greetingsToUser from "..";
+greetingsToUser();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    console.log("Welcome to the Brain Games!")
+    console.log("Welcome to the Brain Games!");
     console.log('');
     const firstName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${firstName}!`);
