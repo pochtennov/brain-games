e6cb1d327c9edfc3659a60fe1fa63787ee3a6ccb
Dmitrii Pochtennov <dmitrii.pochtennov@gmail.com>
2019-01-15 22:26:19 +0100

    Linter errors solved

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ export function isEvenGame() {
     return 'no';
   };
 
-  const round = () => {
+  const round = (firstName) => {
     const randomNumber = Math.floor(Math.random() * 101); // returns a random integer from 0 to 100
     console.log(`Question: ${randomNumber}`);
     const answer = readlineSync.question('Your answer: ');
@@ -27,15 +27,15 @@ export function isEvenGame() {
     console.log(`Try again, ${firstName}`);
     return 0;
   };
-  
+
   console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no"'); 
+  console.log('Answer "yes" if number even otherwise answer "no"');
   const firstName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${firstName}!`);
   let numberOfCorrectRounds = 0;
 
   while (numberOfCorrectRounds < 3) {
-    numberOfCorrectRounds += round();
+    numberOfCorrectRounds += round(firstName);
   }
   console.log(`Congratulations, ${firstName}`);
 }
